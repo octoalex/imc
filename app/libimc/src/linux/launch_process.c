c49f3625482d549425a44af747c9d3f8dc44427d
8//a <123303292+octoalex@users.noreply.github.com>
2025-11-25 00:04:18 +0100

    Cleanup

--- app/libimc/src/linux/launch_process.c
+++ app/libimc/src/linux/launch_process.c
@@ -24,7 +24,6 @@
 #include <dirent.h>
 #include <math.h>
 #include <stdlib.h>
-#include <string.h>
 #include <imc/libimc/launch_process.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
@@ -64,10 +63,12 @@ pid_t launch_process(const char *command[], const char *working_directory, FILE 
     if (process < 0) {
         return PROCESS_ERROR_CREATION_FAIL;
     } else if (process == 0) {
+        // change directory only if a new one is specified
         if (working_directory != nullptr) {
             chdir(working_directory);
         }
 
+        // change files only if necessary, ie either if one is specified or if the one specified is the default
         if (in != nullptr && in != stdin) {
             const int in_fileno = fileno(in);
             dup2(in_fileno, STDIN_FILENO);
@@ -85,39 +86,50 @@ pid_t launch_process(const char *command[], const char *working_directory, FILE 
         // close all other file pointers
         // closing them before copying them will lead to bugs!
         // (there is a reason this was written here)
+        // Fixes Issue #3
         close_all_file_descriptors();
 
+        // change the executed code
         execv(*command, (char * const*)command);
     }
     return process;
 }
 
-int wait_process(pid_t process, bool *was_killed) {
+int wait_process(const pid_t process, bool *was_killed) {
     if (process <= 0) {
         return 0;
     }
 
+    // capture the process if it has either exited or stopped
     siginfo_t status;
     waitid(P_PID, process, &status, WEXITED | WSTOPPED);
 
     if (was_killed != nullptr) {
+        // on *nix, siginfo_t.si_code can describe more information other than if the process terminated correctly,
+        // but for portability, the selection is limited
+        // may express more codes in the future
         *was_killed = status.si_code != CLD_EXITED;
     }
     return status.si_status;
 }
 
-bool is_process_alive(pid_t process) {
+bool is_process_alive(const pid_t process) {
     if (process <= 0) {
         return 0;
     }
 
+    // here's a little trick:
+    // the signal 0 does nothing, but the call still checks if the process is running
+    // so, if it is, the call will be successful and process won't be affected, and if the process is dead, then the
+    // call will fail
     return kill(process, 0) == 0;
 }
 
 // Helpers
 
 static void close_all_file_descriptors() {
-    pid_t self_pid = getpid();
+    const pid_t self_pid = getpid();
+    // represented length as a sum for readability, with comments describing what the length represents
     size_t path_size = 6 /* /proc/ */
         + (int) ceil(log(self_pid + 1)) /* the pid's length in characters */
         + 4 /* /fd/ */
@@ -126,14 +138,13 @@ static void close_all_file_descriptors() {
     char fd_path[path_size];
     sprintf(fd_path, "/proc/%d/fd/", self_pid);
 
-    struct dirent *entry;
-
     // Code based on the one found at https://www.gnu.org/software/libc/manual/html_node/Simple-Directory-Lister.html
+    struct dirent *entry;
     DIR* dp = opendir(fd_path);
     if (dp != nullptr) {
         while ((entry = readdir (dp))) {
             char *error;
-            int fd = (int)strtol(entry->d_name, &error, 10);
+            const int fd = (int)strtol(entry->d_name, &error, 10);
             // a simple fd < 3 could do, but I prefer this approach, as IMO it makes it clearer that I'm only checking
             // for those fds
             if (error == entry->d_name
