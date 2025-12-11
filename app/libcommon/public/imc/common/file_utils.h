bfe35c3eaf5e722174618780d1b75fd5308d792f
8//a <123303292+octoalex@users.noreply.github.com>
2025-12-11 17:37:49 +0100

    Added file utilities

---
+++ app/libcommon/public/imc/common/file_utils.h
@@ -0,0 +1,54 @@
+/* 
+ * This file is part of octoalex's imc (https://github.com/octoalex/imc).
+ * Copyright (c) 2025 octoalex.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Created by octoalex on 11/12/2025.
+ */
+
+#ifndef IMC_FILE_UTILS_H
+#define IMC_FILE_UTILS_H
+
+/// Create a series of nested directories
+/// @param path All the directories to create
+void make_dirs(const char *path);
+
+/// Checks if a path exists and is a directory
+/// @param path The directory to check
+/// @return Whether the path exists and is a directory
+bool is_dir(const char *path);
+
+/// Checks if a path exists and is a file
+/// @param path The file to check
+/// @return Whether the path exists and is a file
+bool is_file(const char *path);
+
+/// Checks if a path exists, is a file and can be read
+/// @param path The file to check
+/// @return Whether the file can be read
+bool can_read(const char *path);
+
+/// Checks if a path exists, is a file and can be written to
+/// @param path The file to check
+/// @return Whether the file can be written to
+bool can_write(const char *path);
+
+/// Checks if a path exists, is a file and can be executed
+/// @param path The file to check
+/// @return Whether the file can be executed
+bool can_execute(const char *path);
+
+#endif //IMC_FILE_UTILS_H
---
+++ app/libcommon/src/linux/file_utils.c
@@ -0,0 +1,74 @@
+/* 
+ * This file is part of octoalex's imc (https://github.com/octoalex/imc).
+ * Copyright (c) 2025 octoalex.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Created by octoalex on 11/12/2025.
+ */
+
+#include <imc/common/file_utils.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <stdlib.h>
+
+constexpr mode_t STANDARD_MODE = 0755;
+
+void make_dirs(const char *path) {
+    char *buffer = calloc(strlen(path) + 1, sizeof(char));
+    char *ptr;
+    size_t offset = 0;
+    do {
+        // get the subdirectory
+        // get the position of the next directory separator
+        ptr = strchr(path + offset, '/');
+        // get the position
+        const size_t end = ptr == nullptr ? strlen(path) : ptr - path;
+        // copy only the new bytes
+        memcpy(buffer + offset, path + offset, end - offset);
+
+        // check if the subdirectory exists
+        if (!is_dir(buffer)) {
+            // if it doesn't, create it
+            mkdir(buffer, STANDARD_MODE);
+        }
+        // set the position of the latest separator as the offset for the next iteration
+        offset = end;
+    } while (ptr != nullptr);
+    // free the buffer
+    free(buffer);
+}
+
+bool is_dir(const char *path) {
+    struct stat st;
+    return stat(path, &st) == 0 && S_ISDIR(st.st_mode);
+}
+
+bool is_file(const char *path) {
+    return access(path, F_OK);
+}
+
+bool can_read(const char *path) {
+    return access(path, W_OK);
+}
+
+bool can_write(const char *path) {
+    return access(path, R_OK);
+}
+
+bool can_execute(const char *path) {
+    return access(path, X_OK);
+}
